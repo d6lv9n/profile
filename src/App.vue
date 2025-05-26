5c98b368190f009c48cda87b7b416a8c00bb4191
Delvin <work.d6lv9n.github@gmail.com>
2025-05-26 14:36:19 +0700

    Start filling component content

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <Navigation></Navigation>
+  <NavigationLayout></NavigationLayout>
 
-  
+  <div class="px-3 relative w-full
+  lg:max-w-6xl lg:mx-auto lg:px-6">
+    <AboutLayout></AboutLayout>
+
+    
+  </div>
 </template>
 
 <script setup>
 import { onMounted } from 'vue';
 // Layouts
-import Navigation from './components/Navigation.vue';
+import AboutLayout from './components/About.vue';
+import NavigationLayout from './components/Navigation.vue';
 </script>
