2a300f7e8db445728921ffac7dd86658499be6cb
Delvin <work.d6lv9n.github@gmail.com>
2025-06-03 15:38:19 +0700

    WelcomeModal added and component updated

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,33 @@
 
     <SkillsLayout></SkillsLayout>
   </div>
+
+  <WelcomeModal
+  v-if="modals.welcome"
+  @close="modals.welcome = false"></WelcomeModal>
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
+import { onBeforeMount, onMounted, reactive } from 'vue';
 // Layouts
 import AboutLayout from './layouts/About/Root.vue';
 import NavigationLayout from './layouts/Navigation.vue';
 import SkillsLayout from './layouts/Skills/Root.vue';
+// Modals
+import WelcomeModal from './modals/Welcome/Root.vue';
+
+// Data
+const modals = reactive({
+  welcome: false,
+});
+
+// Lifecycles
+onBeforeMount(() => {
+  // Check if the welcome modal should be shown
+  if (! localStorage.getItem('hasVisited')) {
+    modals.welcome = true;
+
+    localStorage.setItem('hasVisited', 'true');
+  }
+});
 </script>
